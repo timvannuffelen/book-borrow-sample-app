8c43b7c75d239d7d4a1b2efed5eb2ed3721dc0f1
Tim Vannuffelen <u0068318@thomasmore.be>
2023-08-01 18:45:51 +0200

    Added and configured Serilog + Added MediatR and Blazored.FluentValidation + Added pipeline behavior that logs each MediatR request and response.

---
+++ FictionalBookBorrow/BlazorApp/Behaviors/LoggingPipelineBehavior.cs
@@ -0,0 +1,44 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace BlazorApp.Behaviors;
+
+/// <summary>
+/// A MediatR pipeline behavior that logs each request and response.
+/// </summary>
+/// <typeparam name="TRequest">
+/// Generic argument that represents the type of the request.
+/// </typeparam>
+/// <typeparam name="TResponse">
+/// Generic argument that represents the type of the response.
+/// </typeparam>
+public sealed class LoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger<LoggingPipelineBehavior<TRequest, TResponse>> _logger;
+
+    public LoggingPipelineBehavior(ILogger<LoggingPipelineBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handling {@RequestType} on {@DateTimeUtc}", typeof(TRequest).Name, DateTime.UtcNow);
+
+        var response = await next();
+
+        if (response is IResultBase result && result.IsFailed)
+        {
+            _logger.LogInformation("Errors {@Errors} on {@DateTimeUtc}", result.Errors, DateTime.UtcNow);
+        }
+
+        if (response is Exception)
+        {
+            _logger.LogError("Error {@Exception} on {@DateTimeUtc}", response, DateTime.UtcNow);
+        }
+
+        _logger.LogInformation("Handled {@RequestType} on {@DateTimeUtc}", typeof(TRequest).Name, DateTime.UtcNow);
+
+        return response;
+    }
+}
--- FictionalBookBorrow/BlazorApp/Program.cs
+++ FictionalBookBorrow/BlazorApp/Program.cs
@@ -1,51 +1,120 @@
 using BlazorApp.Areas.Identity;
+using BlazorApp.Behaviors;
 using BlazorApp.Data;
-using Microsoft.AspNetCore.Components;
+using MediatR;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using System.Reflection;
 
-var builder = WebApplication.CreateBuilder(args);
+// Configure an initial "bootstrap" logger to be able to log errors during the startup of the ASP.NET Core host.
+// Note that this bootstrap logger will be completely replaced by the logger configured with "UseSerilog()" below.
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("C:/temp/logs/log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateBootstrapLogger();
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
-builder.Services.AddSingleton<WeatherForecastService>();
+WebApplicationBuilder builder;
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseMigrationsEndPoint();
+    Log.Information("Starting web application...");
+
+    builder = WebApplication.CreateBuilder(args);
+
+
+    // ------------------------------------------------------------------------------------------
+    //                                    Set up logging.
+    // ------------------------------------------------------------------------------------------
+
+    // Create a final logger that will replace the bootstrap logger that was created above. This is the logger that
+    // will be used by the running app. The logger gets its configuration from the appsettings.json file.
+    builder.Host.UseSerilog((context, services, configuration)
+        => configuration.ReadFrom.Configuration(context.Configuration)
+            .ReadFrom.Services(services)
+            .Enrich.FromLogContext());
+
+
+    // ------------------------------------------------------------------------------------------
+    //                               Add services to the container.
+    // ------------------------------------------------------------------------------------------
+
+    RegisterCommonDependencies();
+
+
+    // Add services to the container.
+    string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ApplicationDbContext>();
+    
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+                    // Only enable detailed errors on a production environment for temporary troubleshooting! Setting this value
+                    // to true exposes possibly sensitive error information on the console of your browser. 
+                    //.AddCircuitOptions(options => options.DetailedErrors = true);
+    
+    builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+    builder.Services.AddSingleton<WeatherForecastService>();
+
+    WebApplication app = builder.Build();
+
+
+    // ------------------------------------------------------------------------------------------
+    //                            Configure the HTTP request pipeline.
+    // ------------------------------------------------------------------------------------------
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
+
 }
-else
+catch(Exception exception)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    Log.Fatal(exception, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+// ------------------------------------------------------------------------------------------
+//                                     Helper methods.
+// ------------------------------------------------------------------------------------------
 
-app.UseRouting();
+// Register dependencies that are needed for all environments (Development, Staging, Production).
+void RegisterCommonDependencies()
+{
+    builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingPipelineBehavior<,>));
 
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+    builder.Services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+}
